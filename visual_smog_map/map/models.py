67ea600817d252cc5e541ae6ab353f0ca8da6348
Jakub-Svoboda <jakub.svoboda.developer@gmail.com>
2023-07-30 17:41:02 +0200

    Change latitude and logitude float and secure ranges

--- visual_smog_map/map/models.py
+++ visual_smog_map/map/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Smog(models.Model):
@@ -10,8 +11,12 @@ class Smog(models.Model):
     parcel = models.CharField(max_length=300)
     lv = models.CharField(max_length=300)
     legality_status = models.CharField(max_length=300)
-    latitude = models.CharField(max_length=300)
-    longitude = models.CharField(max_length=300)
+    latitude = models.FloatField(
+        validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)]
+    )  # Latitude range: -180 to 180 degrees
+    longitude = models.FloatField(
+        validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)]
+    )  # Longitude range: -180 to 180 degrees
     cadaster = models.CharField(max_length=500)
     pub_date = models.DateTimeField("date published")
 
